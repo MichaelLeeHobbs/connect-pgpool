d14db89966ae431511c32e01f92076409579bb99
Michael Hobbs <michael.lee.hobbs@gmail.com>
2020-10-17 12:54:14 -0400

    added check to retry if server returns Please try again shortly.

--- tests/loadChannels.js
+++ tests/loadChannels.js
@@ -2,36 +2,60 @@ const fs = require('fs')
 const path = require('path')
 const fetch = require('node-fetch')
 
+const sleep = async (ms = 1) => new Promise(resolve => setTimeout(resolve, ms))
 
-const loadChannels = async (mirthHost)=> {
+const loadChannel = ({mirthHost, Authorization, body}) => fetch(`https://${mirthHost}:8443/api/channels`, {
+    method: 'POST',
+    headers: {Authorization, accept: "application/json", "Content-Type": "application/xml"},
+    body
+})
+    .then(async res => {
+        let result = res.text()
+        if (result.includes('Please try again shortly.')) {
+            await sleep(2000)
+            return loadChannels({mirthHost, Authorization, body})
+        }
+        return result
+    })
+
+const loadChannels = async (mirthHost) => {
     const Authorization = `Basic ${(Buffer.from('admin:admin')).toString('base64')}`
 
     let promises = []
 
     const testInterface = fs.readFileSync(path.resolve(__dirname, '../channels/TestInterface.xml'), 'utf8')
-    promises.push(fetch(`https://${mirthHost}:8443/api/channels`, {method: 'POST', headers: {Authorization, accept: "application/json", "Content-Type": "application/xml"}, body: testInterface})
-        .then(res=>res.text())
-        .then((res)=>console.log('Load testInterface results: ', res))
-    )
+    promises.push(loadChannel({mirthHost, Authorization, body: testInterface}).then((res) =>console.log('Load testInterface results: ', res)))
     // .catch(console.error)
 
     const testRunner = fs.readFileSync(path.resolve(__dirname, '../channels/TestRunner.xml'), 'utf8')
-    promises.push(fetch(`https://${mirthHost}:8443/api/channels`, {method: 'POST', headers: {Authorization, accept: "application/json", "Content-Type": "application/xml"}, body: testRunner})
-        .then(res=>res.text())
-        .then((res)=>console.log('Load testRunner results: ', res))
-    )
+    promises.push(loadChannel({mirthHost, Authorization, body: testRunner}).then((res) =>console.log('Load testInterface results: ', res)))
+    // promises.push(fetch(`https://${mirthHost}:8443/api/channels`, {
+    //         method: 'POST',
+    //         headers: {Authorization, accept: "application/json", "Content-Type": "application/xml"},
+    //         body: testRunner
+    //     })
+    //         .then(res => res.text())
+    //         .then((res) => {
+    //             if (res.includes('Please try again shortly.'))
+    //                 console.log('Load testRunner results: ', res)
+    //         })
+    // )
     // .catch(console.error)
 
 
-    const deployChannels = JSON.stringify({ "set": { "string": [ "3d194c52-0c93-4104-95cb-c4c2baaa8f85", "ac780bfa-1a27-4d37-ba85-5d57c1a4bfdd"] }})
+    const deployChannels = JSON.stringify({"set": {"string": ["3d194c52-0c93-4104-95cb-c4c2baaa8f85", "ac780bfa-1a27-4d37-ba85-5d57c1a4bfdd"]}})
     return Promise.all(promises)
-        .then(()=>{
-            return fetch(`https://${mirthHost}:8443/api/channels/_deploy?returnErrors=true`, {method: 'POST', headers: {Authorization, accept: "application/json", "Content-Type": "application/json"}, body: deployChannels})
-                .then(res=>res.text())
-                .then((res)=>console.log('Start TestInterface results: ', res))
-                .catch((e)=>console.error('Start TestInterface error: ', e))
+        .then(() => {
+            return fetch(`https://${mirthHost}:8443/api/channels/_deploy?returnErrors=true`, {
+                method: 'POST',
+                headers: {Authorization, accept: "application/json", "Content-Type": "application/json"},
+                body: deployChannels
+            })
+                .then(res => res.text())
+                .then((res) => console.log('Start TestInterface results: ', res))
+                .catch((e) => console.error('Start TestInterface error: ', e))
         })
-        .catch(e=>{
+        .catch(e => {
             console.error('Error while loading/deploying channels!', e)
             throw e
         })
