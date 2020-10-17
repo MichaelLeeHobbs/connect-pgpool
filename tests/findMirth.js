56b61b94d98e2bc74101896aa9d7ca77bc0909fc
Michael Hobbs <michael.lee.hobbs@gmail.com>
2020-10-17 12:21:12 -0400

    just a giant wall of spaghetti at this point...

--- tests/findMirth.js
+++ tests/findMirth.js
@@ -1,12 +1,18 @@
 const MIRTH_HOST = process.env.MIRTH_HOST || 'localhost'
 const fetch = require('node-fetch')
 
-const checkMirth = async (mirthHost) => fetch(`http://${mirthHost}:8080`).then(() => true).catch(()=>false)
+const checkMirth = async (mirthHost) => {
+    return fetch(`http://${mirthHost}:8080`).then(() => {
+        console.log(`Found mirth at http://${mirthHost}:8080`)
+        return true
+    }).catch(() => false)
+}
 
 const main = async () => {
-    if(await checkMirth(MIRTH_HOST)) return MIRTH_HOST
-    if(await checkMirth('localhost')) return 'localhost'
-    if(await checkMirth('mirth')) return 'mirth'
+    if ((await checkMirth(MIRTH_HOST))) return MIRTH_HOST
+    if ((await checkMirth('localhost'))) return 'localhost'
+    if ((await checkMirth('mirth'))) return 'mirth'
+    return MIRTH_HOST
 }
 
 module.exports = main
